7e0186a8d592caa175e64ebf33ca41895ec05cca
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2016-06-08 10:23:34 +0100

    Put 4th line of spaces on each ascii number on Haskell implementation

--- AsciiNumberOcr.hs
+++ AsciiNumberOcr.hs
@@ -4,14 +4,14 @@ import Data.List(transpose)
 import Data.List.Split(chunksOf)
 
 translate :: String -> String
-translate " _ | ||_|" = "0"
-translate "     |  |" = "1"
-translate " _  _||_ " = "2"
-translate " _  _| _|" = "3"
-translate "   |_|  |" = "4"
-translate " _ |_  _|" = "5"
-translate " _ |_ |_|" = "6"
-translate " _   |  |" = "7"
-translate " _ |_||_|" = "8"
-translate " _ |_| _|" = "9"
+translate " _ | ||_|   " = "0"
+translate "     |  |   " = "1"
+translate " _  _||_    " = "2"
+translate " _  _| _|   " = "3"
+translate "   |_|  |   " = "4"
+translate " _ |_  _|   " = "5"
+translate " _ |_ |_|   " = "6"
+translate " _   |  |   " = "7"
+translate " _ |_||_|   " = "8"
+translate " _ |_| _|   " = "9"
 translate s = concatMap (translate . concat) $ transpose $ map (chunksOf 3) $ lines s
